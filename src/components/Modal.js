be2af7062a264b880cda3df71755439953bf9ae5
Dinasoa <hei.dinasoa@gmail.com>
2022-06-24 10:44:13 -0900

    feat : adding folders and files

---
+++ src/components/Modal.js
@@ -0,0 +1,11 @@
+export function Modal () {
+    return(
+        <div>
+            <div>
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
